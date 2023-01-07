dfb9bbf9aa72b45b41c2e950571c5afbb3d40640
Rene <rene.najera@gmail.com>
2023-01-07 09:03:04 -0600

    listo

---
+++ thundercracker.py
@@ -0,0 +1,63 @@
+from basepokemon import BasePokemon, BaseMove, Type
+
+class Pokemon(BasePokemon):
+    def __init__(self):
+        BasePokemon.__init__(self)
+        # Has to sum to 100
+        self.spend_hp(0)
+        self.spend_attack(0)
+        self.spend_defence(100)
+        self.add_move(High_voltage())
+        self.add_move(Thunder_attack())
+        self.add_move(Electrocute())
+        self.add_move(Tesla_electricity())
+        self.set_type(Type.Electric)
+        self.move = 0
+        self.moves = ['High_voltage!!', "Thunder_attack!!", "Electrocute!!", "Tesla_electricity!!"]
+
+
+    def get_name(self):
+        return "Thundercracker"
+
+    def choose_move(self, enemy):
+        mov = self.moves[self.move]
+        self.move = self.move + 1 if self.move < len(self.moves) - 1 else 0
+        return self.get_move_by_name(mov)
+
+class High_voltage(BaseMove):
+    def __init__(self):
+        BaseMove.__init__(self)
+        self.choose_uses(1)
+        self.set_type(Type.ELECTRIC)
+
+    def get_name(self):
+        return "High_voltage"
+
+class Thunder_attack(BaseMove):
+    def __init__(self):
+        BaseMove.__init__(self)
+        self.choose_uses(1)
+        self.set_type(Type.ELECTRIC)
+
+    def get_name(self):
+        return "Thunder_attack"
+
+
+class Electrocute(BaseMove):
+    def __init__(self):
+        BaseMove.__init__(self)
+        self.choose_uses(1)
+        self.set_type(Type.FIRE)
+
+    def get_name(self):
+        return "Electrocute!!"
+
+
+class Tesla_electricity(BaseMove):
+    def __init__(self):
+        BaseMove.__init__(self)
+        self.choose_uses(1)
+        self.set_type(Type.ELECTRIC)
+
+    def get_name(self):
+        return "Tesla electricity!"
